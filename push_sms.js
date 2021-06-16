0e69cb526ddde370acfc7f6a63ef4cc7e2e8def6
luponq <neverflex2@gmail.com>
2021-06-16 17:23:42 +0300

    m third vers

---
+++ push_sms.js
@@ -0,0 +1,19 @@
+var divs = document.getElementsByTagName("div")
+
+var pO = document.getElementsByClassName('pOrders')
+
+var jsonObj={
+    property1 : "one",
+    property2 : "two",
+    property3 : "three",
+    property4 : "fourth",
+};
+
+var strBuilder = [];
+for(key in jsonObj) {
+  if (jsonObj.hasOwnProperty(key)) {
+    strBuilder.unshift("Key is " + key + ", value is " + jsonObj[key] + "\n");
+  }
+}
+
+alert(strBuilder.join(""));
